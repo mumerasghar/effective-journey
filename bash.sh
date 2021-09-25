7d7d062cba0a69f55c95c0792e946ae39c1c3099
umer <umerasghar2013@gmail.com>
2021-09-25 18:52:04 +0500

    bash

---
+++ bash.sh
@@ -0,0 +1,17 @@
+
+git clone https://github.com/oaeka/effective-journey.git
+cd effective-journey/
+git checkout local
+mkdir Dataset
+cd Dataset
+
+wget https://github.com/jbrownlee/Datasets/releases/download/Flickr8k/Flickr8k_Dataset.zip
+wget https://github.com/jbrownlee/Datasets/releases/download/Flickr8k/Flickr8k_text.zip
+
+unzip Flickr8k_Dataset.zip
+unzip Flickr8k_text.zip
+rm -rf  __MACOSX
+cd ..
+pip install -r requirements.txt
+python3 data/inception_features.py
+python3 train.py
