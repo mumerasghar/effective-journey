55bc4c6aa090d24a13c806f5a9f090addf3aad88
umer <umerasghar2013@gmail.com>
2022-05-21 11:32:37 +0500

    deployable model

--- deployable_code.py
+++ deployable_code.py
@@ -1,18 +1,7 @@
-
-import os
-import time
-
+from gc import freeze
 import yaml
 import numpy as np
 import tensorflow as tf
-
-from inference import evaluate
-from data import create_dataset
-from Models import SelfAttention
-from Models import MultiHeadedAttention
-from inference import karpathy_inference
-from tensorflow_addons.layers import SpectralNormalization
-
 
 DATASET = 'COCO_RCNN'
 with open('./cfg/cfg.yaml', 'r') as f:
@@ -44,6 +33,56 @@ def point_wise_feed_forward_network(d_model, dff):
         ]
     )
 
+
+def scaled_dot_product_attention(q, k, v, mask):
+    matmul_qk = tf.matmul(q, k, transpose_b=True)
+    dk = tf.cast(tf.shape(k)[-1], tf.float32)
+    scaled_attention_logits = matmul_qk / tf.math.sqrt(dk)
+
+    if mask is not None:
+        scaled_attention_logits += (mask * -1e9)
+
+    attention_weights = tf.nn.softmax(scaled_attention_logits, axis=-1)
+    output = tf.matmul(attention_weights, v)
+
+    return output, attention_weights
+
+
+class MultiHeadedAttention(tf.keras.layers.Layer):
+    def __init__(self, d_model, num_heads):
+        super().__init__()
+        self.num_heads = num_heads
+        self.d_model = d_model
+        assert d_model % self.num_heads == 0
+
+        self.depth = d_model // self.num_heads
+
+        self.wq = tf.keras.layers.Dense(d_model, kernel_initializer='glorot_uniform')
+        self.wk = tf.keras.layers.Dense(d_model, kernel_initializer='glorot_uniform')
+        self.wv = tf.keras.layers.Dense(d_model, kernel_initializer='glorot_uniform')
+        self.dense = tf.keras.layers.Dense(d_model, kernel_initializer='glorot_uniform')
+
+    def split_heads(self, x, batch_size):
+        x = tf.reshape(x, (batch_size, -1, self.num_heads, self.depth))
+        return tf.transpose(x, perm=[0, 2, 1, 3])
+
+    def call(self, v, k, q, mask=None):
+        batch_size = tf.shape(q)[0]
+        q = self.wq(q)
+        k = self.wk(k)
+        v = self.wv(v)
+
+        q = self.split_heads(q, batch_size)
+        k = self.split_heads(k, batch_size)
+        v = self.split_heads(v, batch_size)
+
+        scaled_attention, attention_weights = scaled_dot_product_attention(q, k, v, mask)
+        scaled_attention = tf.transpose(scaled_attention, perm=[0, 2, 1, 3])
+
+        concat_attention = tf.reshape(scaled_attention, (batch_size, -1, self.d_model))
+
+        output = self.dense(concat_attention)
+        return output, attention_weights
 
 class EncoderLayer(tf.keras.layers.Layer):
     def __init__(self, d_model, num_heads, dff, rate=0.1):
@@ -198,4 +237,6 @@ params = {
 }
 
 transformer = Transformer(**params)
-transformer.load_weights('')
+transformer.load_weights('current_weights')
+transformer.trainable = False
+
